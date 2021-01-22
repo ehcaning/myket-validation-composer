1c55df308a0080d159dd1f3236872b8b82023c44
Ehsan <ehcan@icloud.com>
2021-01-22 18:12:31 +0330

    fix bugs

--- src/Validator.php
+++ src/Validator.php
@@ -31,10 +31,6 @@ class Validator {
             curl_setopt_array($curl, [
                 CURLOPT_URL => 'https://developer.myket.ir/api/applications/' . $this->packageName . '/purchases/products/' . $sku . '/tokens/' . $payment_token,
                 CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_HEADER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
                 CURLOPT_FOLLOWLOCATION => true,
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                 CURLOPT_CUSTOMREQUEST => 'GET',
@@ -43,7 +39,7 @@ class Validator {
                 ],
             ]);
 
-            $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
             $response = curl_exec($curl);
 
             curl_close($curl);
@@ -55,7 +51,7 @@ class Validator {
                 'data' => $response,
             ];
         } catch (\Throwable $th) {
-            throw new Exception("Can't validate Myket Token");
+            throw new \Exception("Can't validate Myket Token");
         }
     }
 }
